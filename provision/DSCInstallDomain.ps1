307cd86b41b9c3b88cac14df4023e259bc8e0027
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-03 16:14:25 +0200

    Create DSCInstallDomain.ps1

---
+++ provision/DSCInstallDomain.ps1
@@ -0,0 +1,54 @@
+Configuration ADDomain_NewForest_Config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $SafeModePassword
+    )
+    Import-DscResource -ModuleName PSDscResources
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+    Import-DscResource -ModuleName ComputerManagementDsc
+    Node 'localhost'
+    {
+        WindowsFeature 'Active Directory Domain Services'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+        WindowsFeature 'RSAT-ADDS'
+        {
+            Name   = 'RSAT-ADDS'
+            Ensure = 'Present'
+        }
+        
+        ADDomain 'party.hard'
+        {
+            DomainName                    = 'vipnet.local'
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
+#Next block is using to allow password as plain text
+$cd = @{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost'
+            PSDscAllowPlainTextPassword = $true
+         }
+    )
+}
+#Define user and password for ADDomain deployment (also used for restore).
+$password = ConvertTo-SecureString "RestorePassword123$" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential('vagrant',$password)
+#Create MOF
+ADDomain_NewForest_Config -Credential $cred -SafeModePassword $cred -ConfigurationData $cd
+#Execute MOF
+Start-DscConfiguration -Path .\ADDomain_NewForest_Config -Force -Wait -Verbose
