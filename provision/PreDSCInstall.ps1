d737b86dade72554f31512a1e0410f367ffd0459
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-03 16:13:16 +0200

    Create PreDSCInstall.ps1

---
+++ provision/PreDSCInstall.ps1
@@ -0,0 +1,4 @@
+Install-Module -Name ActiveDirectoryDsc -Force
+Install-Module -Name PSDscResources -Force
+Install-Module -Name ComputerManagementDsc -Force
+Install-Module -Name xDhcpServer -Force
