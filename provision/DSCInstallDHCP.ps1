fbdacb004ed25fa141f6dce6c099eb57f89e1b36
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-15 17:45:57 +0200

    Update DSCInstallDHCP.ps1

--- provision/DSCInstallDHCP.ps1
+++ provision/DSCInstallDHCP.ps1
@@ -1,7 +1,7 @@
 Configuration xDhcpsServerScope_NewScope
 {
     Import-DscResource -ModuleName xDHCpServer
-    Import-DscResource -ModuleName xDhcpServerOption
+    Import-DscResource -Name xDhcpServerOption
     #Define NIC IP
     $IP = Get-NetIPAddress -InterfaceAlias "Ethernet 2" | Where-Object {$_.IPAddress -notlike "*:*" } | select -ExpandProperty IPAddress
     Node 'localhost'
